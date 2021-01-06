28f991d46e5c746aeb4def546cf5d134e5b6246e
yioda <jyc180g@gmail.com>
2021-01-06 15:17:56 +0800

    Add backup func

---
+++ aitsc.py
@@ -0,0 +1,5 @@
+from src.main import main
+
+
+if __name__ == '__main__':
+    main()
--- src/main.py
+++ src/main.py
@@ -5,8 +5,9 @@ from sshconf import read_ssh_config, empty_ssh_config_file
 from os.path import expanduser
 from argparse import ArgumentParser
 from argparse import RawTextHelpFormatter
-from pkg_resources import get_distribution
+# from pkg_resources import get_distribution
 from shutil import copyfile
+from datetime import datetime
 
 
 def get_args():
@@ -14,59 +15,73 @@ def get_args():
     # parser.add_argument('-v', '--version', action='version',
     #                     version=get_distribution('ansible-inventory-to-ssh-config').version)
     parser.add_argument("inventory_file", help="ansible inventory file")
-    parser.add_argument("-o", "--output", help="ssh config output path (default: ~/.ssh/newconfig)", default="~/.ssh/newconfig")
+    parser.add_argument("-o", "--output", help="ssh config output path (default: ~/.ssh/config)", default="~/.ssh/config")
     parser.add_argument("-d", "--dry-run", help="show new configurations without updating file", action="store_true")
-    parser.add_argument("--without-backup", help="show new configurations without updating file", action="store_true")
+    parser.add_argument("--with-backup", help="backup config file before updating(default: true)", action="store_true", default=True)
 
     return parser.parse_args()
 
-def ansible_inventory_to_ssh_config(inventory_file, output, dry_run=False):
-    loader = DataLoader()
-    inventory = InventoryManager(loader=loader, sources=[inventory_file])
-    variable_manager = VariableManager(loader=loader, inventory=inventory)
 
-    print("Inventory: {}".format(inventory_file))
-    print("Target: {}".format(output))
-
-
-    try:
-        ssh_config = read_ssh_config(output)
-        # copyfile(output, "")
-    except FileNotFoundError:
-        print("No such file, generate new file: {} ...".format(output))
-        ssh_config = empty_ssh_config_file()
-
-    for host in inventory.get_hosts('web'):
-        host_var = variable_manager.get_vars(host=host)
+def update_ssh_config(ssh_config, inventories, variables, group='all'):
+    for host in inventories.get_hosts(group):
+        host_var = variables.get_vars(host=host)
 
         try:
             address = host_var['ansible_ssh_host']
-        except:
+        except KeyError:
             try:
                 address = host_var['ansible_host']
-            except:
+            except KeyError:
                 print('Failed to get [{}] ssh address... '.format(host))
                 continue
 
-        if ssh_config.host(host.get_name()):
+        try:
             ssh_config.set(host.get_name(), HostName=address)
-        else:
+        except ValueError:
             ssh_config.add(host.get_name(), HostName=address)
 
-    if dry_run:
-        for h in ssh_config.hosts():
-            print(h, ssh_config.host(h))
-    else:
-        try:
+
+def backup(target_file):
+    copyfile(target_file, "{}.{}".format(target_file, datetime.now().strftime("%Y%m%d_%H%M%S")))
+
+
+def print_ssh_config(ssh_config):
+    for h in ssh_config.hosts():
+        print(h, ssh_config.host(h))
+
+
+def ansible_inventory_to_ssh_config(inventory_file, output, dry_run=False, with_backup=False):
+    print("Inventory: {}".format(inventory_file))
+    print("Target: {}".format(output))
+
+    loader = DataLoader()
+    inventories = InventoryManager(loader=loader, sources=[inventory_file])
+    variables = VariableManager(loader=loader, inventory=inventories)
+
+    try:
+        ssh_config = read_ssh_config(output)
+        update_ssh_config(ssh_config, inventories, variables)
+
+        if with_backup:
+            backup(output)
+
+        if dry_run:
+            print_ssh_config(ssh_config)
+        else:
             ssh_config.save()
-        except AttributeError:
+    except FileNotFoundError:
+        print("No such file, generate a new file: {} ...".format(output))
+
+        ssh_config = empty_ssh_config_file()
+        update_ssh_config(ssh_config, inventories, variables)
+
+        if dry_run:
+            print_ssh_config(ssh_config)
+        else:
             ssh_config.write(output)
 
 
 def main():
     args = get_args()
-    ansible_inventory_to_ssh_config(args.inventory_file, expanduser(args.output), args.dry_run)
+    ansible_inventory_to_ssh_config(args.inventory_file, expanduser(args.output), args.dry_run, args.with_backup)
 
-
-if __name__ == '__main__':
-    main()
