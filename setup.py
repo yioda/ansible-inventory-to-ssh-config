f03e4419362bb8e945614c57ef0dc94e5de3b660
yioda <jyc180g@gmail.com>
2021-01-11 12:05:35 +0800

    Update version and specify denpencies' version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ansible-inventory-to-ssh-config",
-    version="0.0.1",
+    version="0.0.2",
     description="Generate ssh config file from Ansible inventory",
     author="Yioda",
     author_email='jyc180g@gmail.com',
@@ -15,8 +15,8 @@ setup(
         ],
     },
     install_requires=[
-        'ansible',
-        'sshconf'
+        'ansible==2.10.5',
+        'sshconf==0.2.2'
     ],
     python_requires=">=3"
 )
